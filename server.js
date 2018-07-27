833658935d6aa90b0436c7d8aa4d59194acab18d
Tiago Cabral <150150296@aluno.unb.br>
2018-07-26 21:21:42 -0300

    Colocando segredos no lugar da url

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
-import axios from 'axios';
-import {RequestUrl} from "./secrets";
+const axios = require('axios');
+const Secrets = require('./secrets');
 
 // Times in ms
-SLEEP_TIME = 3000;
+SLEEP_TIME = 15000;
 ERROR_SLEEP_TIME = 60000;
 MAX_TIMEOUT_WAIT = 15000;
 
 const requestFunction = () => {
 
-    axios.get(RequestUrl
-        ,
+    console.log("Fazendo request...");
+    axios.get(Secrets.RequestUrl,
         {timeout: MAX_TIMEOUT_WAIT}
     )
         .then(response => {
-            console.log(response.data.message);
-            console.log(`Esperando ${SLEEP_TIME/1000} segundo(s).`);
+            console.log(response.data.message + ` Esperando ${SLEEP_TIME/1000} segundo(s).`);
             setTimeout(requestFunction, SLEEP_TIME);
         })
         .catch(err => {
